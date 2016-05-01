502d6b71b3f8570c50403952920ee6de10b529f7
Michael Jeffrey Wu <michaeljeffreywu@gmail.com>
2016-05-01 06:49:00 -0400

    prettify everything

--- www/js/controllers/chats-controller.js
+++ www/js/controllers/chats-controller.js
@@ -1,8 +1,12 @@
 angular.module('chats-controller', [])
   
-.controller('ChatsCtrl', function ($scope, Rooms, Chats, $state) {
+.controller('ChatsCtrl', function ($scope, Rooms, Chats, $state, $ionicHistory) {
     //console.log("Rooms Controller initialized");
     $scope.rooms = Rooms.all();
+
+    $scope.goHome = function() {
+      $ionicHistory.goBack();
+    }
 
     $scope.openChatRoom = function (roomId) {
         $state.go('chat', {
--- www/js/controllers/onboard-controller.js
+++ www/js/controllers/onboard-controller.js
@@ -2,70 +2,68 @@ angular.module('onboard-controller', [])
 
 .controller('OnboardCtrl', function($scope, $ionicHistory, $rootScope, $firebaseArray, $cordovaCamera, $http, $state, $ionicLoading) {
 
-	$scope.user = {
-		fullName: '',
-		gender: '',
-		age: 0,
-		description: '',
-		image: null,
-		marvelImage: '',
-		userId: $rootScope.userData.uid
-	}
+    $scope.user = {
+        fullName: '',
+        gender: '',
+        age: 0,
+        description: '',
+        image: null,
+        marvelImage: '',
+        userId: $rootScope.userData.uid
+    }
 
-	$ionicHistory.clearHistory();
+    $ionicHistory.clearHistory();
 
+    var getMarvel = function() {
+        var marvelApiKey = '8840657df7518a8f93eb0ece030f5091';
 
+        var randomChar = $rootScope.marvelChars[Math.floor(Math.random() * $rootScope.marvelChars.length)];
 
-  var getMarvel = function() {
-  		var marvelApiKey = '8840657df7518a8f93eb0ece030f5091';
+        $http({
+            method: 'GET',
+            url: 'http://gateway.marvel.com/v1/public/characters/' + randomChar,
+            params: { apikey: marvelApiKey }
+        }).then(function successCallback(response) {
+            $scope.user.marvelImage = response.data.data.results[0].thumbnail.path + '.' + response.data.data.results[0].thumbnail.extension;
+            // console.log(response);
+        }, function errorCallback(response) {
+            console.log('Marvel API Error: ', response);
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
 
-	var randomChar = $rootScope.marvelChars[Math.floor(Math.random() * $rootScope.marvelChars.length)];
-    // $http.get('http://gateway.marvel.com/v1/public/characters?apikey=8840657df7518a8f93eb0ece030f5091');
-    $http({
-      method: 'GET',
-      url: 'http://gateway.marvel.com/v1/public/characters/' + randomChar,
-      params: {apikey: marvelApiKey}
-    }).then(function successCallback(response) {
-    	$scope.user.marvelImage = response.data.data.results[0].thumbnail.path + '.' + response.data.data.results[0].thumbnail.extension;
-      // console.log(response);
-    }, function errorCallback(response) {
-    	console.log('Marvel API Error: ', response);
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-    });
-  }
+    $scope.submitForm = function(user) {
+        $ionicLoading.show();
+        getMarvel();
 
-	$scope.submitForm = function(user) {
-		$ionicLoading.show();
-	  getMarvel();
+        var ref = new Firebase('https://marvelify.firebaseio.com/users');
+        var sync = $firebaseArray(ref);
+        console.log(user);
+        sync.$add({
+            fullName: user.fullName,
+            gender: user.gender,
+            age: user.age,
+            description: user.description,
+            image: user.image,
+            marvelImage: user.marvelImage,
+            userId: user.userId
+        }).then(function(ref) {
+            var id = ref.key();
+            console.log("added record with id " + id);
+            sync.$indexFor(id); // returns location in the array
+            $ionicLoading.hide();
+            $state.go('home');
+        });
+        // sync.$set({ email: user.email, provider: user.provider });
+    }
 
-	var ref = new Firebase('https://marvelify.firebaseio.com/users');
-	var sync = $firebaseArray(ref);
-		console.log(user);
-		sync.$add({
-                    fullName: user.fullName,
-                    gender: user.gender,
-                    age: user.age,
-                    description: user.description,
-                    image: user.image,
-                    marvelImage: user.marvelImage,
-                    userId: user.userId
-                }).then(function(ref) {
-  var id = ref.key();
-  console.log("added record with id " + id);
-  sync.$indexFor(id); // returns location in the array
-  $ionicLoading.hide();
-  $state.go('home');
-});
-  	// sync.$set({ email: user.email, provider: user.provider });
-	}
-
-	 $scope.upload = function() {
+    $scope.upload = function() {
         var options = {
-            quality : 75,
-            destinationType : Camera.DestinationType.DATA_URL,
-            sourceType : Camera.PictureSourceType.CAMERA,
-            allowEdit : true,
+            quality: 75,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            allowEdit: true,
             encodingType: Camera.EncodingType.JPEG,
             popoverOptions: CameraPopoverOptions,
             targetWidth: 500,
@@ -73,7 +71,7 @@ angular.module('onboard-controller', [])
             saveToPhotoAlbum: false
         };
         $cordovaCamera.getPicture(options).then(function(imageData) {
-        	$scope.user.image = imageData;
+            $scope.user.image = imageData;
         }, function(error) {
             console.error(error);
         });
