ec086c1c8be7902651920748b04daf6b6854855c
Michael Jeffrey Wu <michaeljeffreywu@gmail.com>
2016-04-30 22:50:12 -0400

    marvel API integration working

--- www/js/app.js
+++ www/js/app.js
@@ -58,6 +58,8 @@ angular.module('marvelify', ['ionic',
                 $location.path("/login");
             }
         });
+
+        $rootScope.marvelChars = [1009610, 1009351, 1009220, 1009368, 1009189, 1009664, 1010809, 1009524, 1009268, 1009562]
     });
 })
 
--- www/js/controllers/onboard-controller.js
+++ www/js/controllers/onboard-controller.js
@@ -1,18 +1,41 @@
 angular.module('onboard-controller', [])
 
-.controller('OnboardCtrl', function($scope, $ionicHistory, $rootScope, $firebaseArray, $cordovaCamera) {
+.controller('OnboardCtrl', function($scope, $ionicHistory, $rootScope, $firebaseArray, $cordovaCamera, $http, $state) {
 
 	$scope.user = {
 		fullName: '',
 		gender: '',
 		age: 0,
 		description: '',
-		image: null
+		image: null,
+		marvelImage: ''
 	}
 
 	$ionicHistory.clearHistory();
 
+
+
+  var getMarvel = function() {
+  		var marvelApiKey = '8840657df7518a8f93eb0ece030f5091';
+
+	var randomChar = $rootScope.marvelChars[Math.floor(Math.random() * $rootScope.marvelChars.length)];
+    // $http.get('http://gateway.marvel.com/v1/public/characters?apikey=8840657df7518a8f93eb0ece030f5091');
+    $http({
+      method: 'GET',
+      url: 'http://gateway.marvel.com/v1/public/characters/' + randomChar,
+      params: {apikey: marvelApiKey}
+    }).then(function successCallback(response) {
+    	$scope.user.marvelImage = response.data.data.results[0].thumbnail.path + '.' + response.data.data.results[0].thumbnail.extension;
+      // console.log(response);
+    }, function errorCallback(response) {
+    	console.log('Marvel API Error: ', response);
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+    });
+  }
+
 	$scope.submitForm = function(user) {
+	  getMarvel();
 
 	var ref = new Firebase('https://marvelify.firebaseio.com/users/'+ $rootScope.userData.uid);
 	var sync = $firebaseArray(ref);
@@ -22,11 +45,14 @@ angular.module('onboard-controller', [])
                     gender: user.gender,
                     age: user.age,
                     description: user.description,
-                    image: user.image
+                    image: user.image,
+                    marvelImage: user.marvelImage
                 }).then(function(ref) {
   var id = ref.key();
   console.log("added record with id " + id);
   sync.$indexFor(id); // returns location in the array
+
+  $state.go('home');
 });
   	// sync.$set({ email: user.email, provider: user.provider });
 	}
